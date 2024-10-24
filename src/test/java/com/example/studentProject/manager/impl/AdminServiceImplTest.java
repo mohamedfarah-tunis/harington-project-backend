116d784cb2bdbf4471004b06f4dc12dac242691b
mohamed farah <mohamedsalah.farah.ext@alten.com>
2024-10-24 16:28:44 +0200

    add liquibase to the project

---
+++ src/test/java/com/example/studentProject/manager/impl/AdminServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.example.studentProject.manager.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
